b59bd96700abc3ac4c532445b6a866501341ac36
Orange Juice <orangejuiceextension@gmail.com>
2026-01-23 01:21:40 -0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+const titleRegex = /<title>(.*?)<\/title>/;
+
+const getHtmlTitle = async (url: string): Promise<string | undefined> => {
+	const fixedUrl: string = url;
+	if (!(url.startsWith('http') || url.startsWith('/'))) {
+		return undefined;
+	}
+
+	const response = await fetch(fixedUrl, { cache: 'force-cache' });
+	const html = await response.text();
+	if (!html) {
+		return undefined;
+	}
+	const match = titleRegex.exec(html);
+	if (match && match.length >= 1) {
+		return match[1];
+	}
+};
+
+export default {
+	async fetch(request, _env, _ctx): Promise<Response> {
+		const url = URL.parse(request.url);
+		if (url?.pathname.startsWith('/title')) {
+			const urlParam = url?.searchParams.get('url') ?? '';
+			return Response.json({ title: await getHtmlTitle(urlParam) });
+		}
+		return new Response('fail');
+	},
+} satisfies ExportedHandler<Env>;
